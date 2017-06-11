758b730a629ff572f0927f278535f25bd6d47a60
EAGLE <apowerz007@hotmail.com>
2017-06-11 14:59:48 -0400

    Added semicolons to the end of the constructors

--- js/constructors.js
+++ js/constructors.js
@@ -5,10 +5,10 @@ var NbrData = function(data) {
     this.info = data.info;
     this.location = data.location;
     this.show = ko.observable(JSON.parse(data.show)); // parses and converts into an object corresponding to the given text
-}
+};
 
 // photo constructor
 var Photo = function(data) {
     this.url = '<img src="'+data.images.low_resolution.url+'" alt="Instagram photo" width="100" height="100">';
     this.caption = data.caption.text;
-}
+};
